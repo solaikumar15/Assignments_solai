359f29484280edd22dae8ca227ceb45145907508
“solai” <solai.muthukumar@gmail.com>
2019-03-12 19:48:06 -0400

    added new class secondAssignmentNew

---
+++ Assignments/src/day11/SecondAssignmentNew.java
@@ -0,0 +1,13 @@
+package day11;
+
+public class SecondAssignmentNew {
+	
+	public static void state() {
+		System.out.println("California is a state in USA");
+	}
+
+	public static void main(String[] args) {
+		state();
+	}
+	
+}
