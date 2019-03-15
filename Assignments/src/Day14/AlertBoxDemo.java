ee9dd5df914ce5d0cdb592828adccb9449bccb69
“solai” <solai.muthukumar@gmail.com>
2019-03-15 17:26:28 -0400

    Checking in the code for Day 14 assignments

---
+++ Assignments/src/Day14/AlertBoxDemo.java
@@ -0,0 +1,27 @@
+package Day14;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class AlertBoxDemo {
+	public static void main(String[] args) {
+//		Open the browser
+		WebDriver driver = new ChromeDriver();
+//		Maximize the Window
+		driver.manage().window().maximize();
+
+//		Navigate to the application
+		driver.get("https://whitecircleschool.com/alertboxdemo/");
+
+//		Click on Try Button
+		driver.findElement(By.cssSelector("body > div.elementor.elementor-599 > div > div > section.elementor-element.elementor-element-9860042.elementor-section-boxed.elementor-section-height-default.elementor-section-height-default.elementor-section.elementor-top-section > div > div > div > div > div > div > div > button")).click();
+
+//		Click on OK button
+		
+		driver.switchTo().alert().accept();
+		
+	}
+
+
+}
