4d2607517a94c6dbc6d1def33da0ec96cdb66bcb
“solai” <solai.muthukumar@gmail.com>
2019-03-15 18:22:19 -0400

    sync with master

---
+++ Assignments/src/Day13/Assignment1.java
@@ -0,0 +1,18 @@
+package Day13;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Assignment1 {
+	public static void main(String[] args) {
+WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://seleniumhq.github.io/selenium/docs/api/java/");
+		
+		driver.manage().window().fullscreen();
+		
+		
+		
+	}
+
+}
