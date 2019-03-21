059852d9cc3f278887177c824f88b7c88ef8c6e7
“solai” <solai.muthukumar@gmail.com>
2019-03-20 20:25:46 -0400

    changes done

--- Assignments/src/Day14/CategoriesAndHobbies.java
+++ Assignments/src/Day14/CategoriesAndHobbies.java
@@ -13,19 +13,18 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class CategoriesAndHobbies {
 	static WebDriver driver;
-
 	public static boolean advPresent() {
-
+		
 		boolean advVisible;
 		try {
-			advVisible = driver.findElement(By.xpath("(//button[contains(@data-click,'close')])[1]")).isDisplayed();
-
-		} catch (Exception e) {
-			advVisible = false;
-		}
+		advVisible = driver.findElement(By.xpath("(//button[contains(@data-click,'close')])[1]")).isDisplayed();
+		
+	} catch (Exception e) {
+		advVisible = false;
+	}
 		return advVisible;
 	}
-
+	
 	public static void main(String[] args) throws InterruptedException {
 //		Open the browser		
 		driver = new ChromeDriver();
@@ -34,30 +33,28 @@ public class CategoriesAndHobbies {
 
 //		Navigate to the application
 		driver.get("https://www.barnesandnoble.com/");
-
+		
 //		Click on I'll Pass button in the advertisement page to close
 		if (advPresent()) {
-			driver.findElement(By.xpath("(//button[contains(@data-click,'close')])[1]")).click();
+		driver.findElement(By.xpath("(//button[contains(@data-click,'close')])[1]")).click();
 		}
-
+		
 //		Hover the mouse over Gift,Home&Office Tab
-		WebDriverWait wait = new WebDriverWait(driver, 10);
-		wait.until(ExpectedConditions.invisibilityOfElementLocated(By.className("expanded")));
-		WebElement giftHomeandOffice = driver.findElement(By.linkText("Gift, Home & Office"));
-		Actions action = new Actions(driver);
+		WebElement giftHomeandOffice= driver.findElement(By.cssSelector("#topNav10 > a"));
+		Actions action = new Actions (driver);
 		action.moveToElement(giftHomeandOffice).perform();
-
+		
 ////	Click on Hobbies link from the Categories List
-
+		WebDriverWait wait = new WebDriverWait(driver,30);
 		wait.until(ExpectedConditions.elementToBeClickable(By.linkText("Hobbies")));
-
+		
 		driver.findElement(By.linkText("Hobbies")).click();
 
 //		Select "Best Sellers" from the Sort By drop down list
-		wait.until(ExpectedConditions.elementToBeClickable(By.id("sortProducts1-replacement")));
-		driver.findElement(By.id("sortProducts1-replacement")).click();
-		driver.findElement(By.linkText("Highly Rated")).click();
-
+	    WebElement sortBy = driver.findElement(By.id("sortProducts1-button-label"));
+	     Select select = new Select(sortBy);
+         select.selectByVisibleText("Oldest to Newest");
+		
 	}
 //This also is correct
 }
