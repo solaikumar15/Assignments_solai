0d8d432e9b539c88785fc1ac9e806c8f70d56072
“solai” <solai.muthukumar@gmail.com>
2019-03-20 22:46:33 -0400

    updated the CBT assignment`

--- Assignments/src/Day17/CBTAssignment.java
+++ Assignments/src/Day17/CBTAssignment.java
@@ -13,47 +13,40 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
-
-
-
 public class CBTAssignment {
 	WebDriver driver;
-	
+
 	@BeforeTest
 	@Parameters("browser")
 	public void openbrowser(String browser) {
-		
-	if(browser.equals("IE") ) {
-	System.setProperty("webdriver.ie.driver", "IEDriverServer.exe");
-	driver = new InternetExplorerDriver();
+
+		if (browser.equals("IE")) {
+			System.setProperty("webdriver.ie.driver", "IEDriverServer.exe");
+			driver = new InternetExplorerDriver();
+		} else if (browser.equals("Chrome")) {
+			driver = new ChromeDriver();
+		} else if (browser.equals("Firefox")) {
+			FirefoxOptions options = new FirefoxOptions();
+			options.setCapability("marionette", false);
+			System.setProperty("webdriver.gecko.driver", "geckodriver.exe");
+			driver = new FirefoxDriver();
+		} else if (browser.equals("Edge")) {
+			System.setProperty("webdriver.edge.driver", "MicrosoftWebDriver.exe");
+			driver = new EdgeDriver();
+		}
+		driver.manage().window().maximize();
+		driver.get("https://www.linkedin.com");
 	}
-	else if(browser.equals("Chrome")) {
-		driver = new ChromeDriver();
-	}
-	else if(browser.equals("Firefox")) {
-		FirefoxOptions options = new FirefoxOptions();
-		options.setCapability("marionette", false);
-		System.setProperty("webdriver.gecko.driver", "geckodriver.exe");	
-		driver = new FirefoxDriver();
-	}
-	else if(browser.equals("Edge")) {
-		System.setProperty("webdriver.edge.driver", "MicrosoftWebDriver.exe");
-		driver = new EdgeDriver();
-	}
-	driver.manage().window().maximize();
-	driver.get("https://www.linkedin.com");
-	}
-	
+
 	@Test
 	public void verifySignInBtnStatus() {
-	boolean signinBtnStatus	= driver.findElement(By.id("login-submit")).isDisplayed();
-	Assert.assertTrue(signinBtnStatus);
-}
-//	@AfterTest
-//	public void closeBrowser() {
-//		driver.close();
-//	}
+		boolean signinBtnStatus = driver.findElement(By.id("login-submit")).isEnabled();
+		Assert.assertFalse(signinBtnStatus);
+	}
 
-
+	@AfterTest
+	public void closeBrowser() {
+		driver.close();
+	}
 
 }
